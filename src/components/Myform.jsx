f0987f1a8242a3469f971f7ad5595b40ea3f45f8
MateusMiranda20 <mateuusmiranda20@gmail.com>
2025-06-20 20:50:22 -0300

    project in progress.

--- src/components/Myform.jsx
+++ src/components/Myform.jsx
@@ -1,27 +1,81 @@
 import './Myform.css'
 
+import { useState } from 'react'
+import { useFetch } from '../hooks/useFetch';
+
+const url = 'http://localhost:5000/products';
+
+
 const Myform = () => {
+
+    //custom hook para buscar dados
+    const {data: items} = useFetch(url);    
+
+    const [name, setName] = useState('')
+    const [price, setPrice] = useState('')
+
+    //useEffect(() => {
+       // const fetchProducts = async () => {
+         //   try {
+          //      const response = await fetch(url)
+           //     const data = await response.json()
+            //    setProducts(data)
+           // } catch (error) {
+          //      console.error('Error fetching products:', error)
+           // }
+      //  }
+
+       // fetchProducts()
+    //}, [])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const product = { name, price };
+
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(product)
+        })
+
+        //Carregamento dinâmico de produtos
+        const addedProduct = await res.json();
+        setProducts((prevProducts) => [...prevProducts, addedProduct]);
+
+        setName('');
+        setPrice('');
+    }
+
+
+
     return (
-        <div>
-            <h1>Forms</h1>
-            <form>
-                <div>
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" id="name" name="name" required />
-                </div>
-
-                <div>
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" required />
-                </div>
-
-                <div>
-                    <label htmlFor="message">Message:</label>
-                    <textarea id="message" name="message" rows="4" required></textarea> 
-                </div>
-
-                <button type="submit">Submit</button>
-            </form>
+        <div className='app'>
+            <div className='product-list'>
+                <h1>Lista de Produtos</h1>
+                <ul>
+                    {items && items .map(product => (
+                        <li key={product.id}>
+                            {product.name} - R$: {product.price}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className='add-product'>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Nome:
+                        <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)} />
+                    </label>
+                    <label>
+                        Preço:
+                        <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)} />
+                    </label>
+                    <input type="submit" value="Adicionar Produto" />
+                </form>
+            </div>
         </div>
     )
 }
